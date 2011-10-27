5a256ab56518d7c5bd1fb73c683f809137f3c1e2
Robert Kaye <rob@musicbrainz.org>
2011-10-26 21:37:02 -0700

    Querying the DB for redirect information now works

--- coverart_redirect/request.py
+++ coverart_redirect/request.py
@@ -21,112 +21,6 @@ class CoverArtRedirect(object):
         self.cmd = None
         self.proto = None
 
-    def handle_cmd_cddb_query(self):
-        """Perform a CD search based on either the FreeDB DiscID or the CD TOC."""
-        if len(self.cmd) < 3:
-            return ["500 Command syntax error."]
-
-        discid = self.cmd[0]
-        try:
-            int(discid, 16)
-        except ValueError:
-            return ["500 ID not hex."]
-
-        try:
-            num_tracks = int(self.cmd[1])
-        except ValueError:
-            return ["500 Command syntax error."]
-
-        if len(self.cmd) < 3 + num_tracks:
-            return ["500 Command syntax error."]
-
-        offsets = []
-        for i in xrange(2, 2 + num_tracks):
-            offsets.append(int(self.cmd[i]))
-        offsets.append(int(self.cmd[2 + num_tracks]) * 75)
-
-        durations = []
-        for i in xrange(num_tracks):
-            durations.append((offsets[i + 1] - offsets[i]) * 1000 / 75)
-
-        toc_query = """
-            SELECT DISTINCT
-                m.id,
-                CASE
-                    WHEN (SELECT count(*) FROM medium WHERE release = r.id) > 1 THEN
-                        rn.name || ' (disc ' || m.position::text || ')'
-                    ELSE
-                        rn.name
-                END AS title,
-                CASE
-                    WHEN artist_name.name = 'Various Artists' THEN
-                        'Various'
-                    ELSE
-                        artist_name.name
-                END AS artist
-            FROM
-                medium m
-                JOIN tracklist t ON t.id = m.tracklist
-                JOIN tracklist_index ti ON ti.tracklist = t.id
-                JOIN release r ON m.release = r.id
-                JOIN release_name rn ON r.name = rn.id
-                JOIN artist_credit ON r.artist_credit = artist_credit.id
-                JOIN artist_name ON artist_credit.name = artist_name.id
-            WHERE
-                toc <@ create_bounding_cube(%(durations)s, %(fuzzy)s::int) AND
-                track_count = %(num_tracks)s
-        """
-
-        discid_query = """
-            SELECT DISTINCT
-                m.id,
-                CASE
-                    WHEN (SELECT count(*) FROM medium WHERE release = r.id) > 1 THEN
-                        rn.name || ' (disc ' || m.position::text || ')'
-                    ELSE
-                        rn.name
-                END AS title,
-                CASE
-                    WHEN artist_name.name = 'Various Artists' THEN
-                        'Various'
-                    ELSE
-                        artist_name.name
-                END AS artist
-            FROM
-                medium m
-                JOIN medium_cdtoc mc ON m.id = mc.medium
-                JOIN cdtoc c ON c.id = mc.cdtoc
-                JOIN tracklist t ON t.id = m.tracklist
-                JOIN release r ON m.release = r.id
-                JOIN release_name rn ON r.name = rn.id
-                JOIN artist_credit ON r.artist_credit = artist_credit.id
-                JOIN artist_name ON artist_credit.name = artist_name.id
-            WHERE
-                c.freedb_id = %(discid)s AND
-                t.track_count = %(num_tracks)s
-        """
-
-        #used_toc = False
-        #rows = self.conn.execute(discid_query, dict(discid=discid, num_tracks=num_tracks)).fetchall()
-        #if not rows:
-        used_toc = True
-        rows = self.conn.execute(toc_query, dict(durations=durations, num_tracks=num_tracks, fuzzy=10000)).fetchall()
-
-        if not rows:
-            return ["202 No match found."]
-
-        # Only one match and we didn't use the TOC
-        if len(rows) == 1 and not used_toc:
-            id, title, artist = rows[0]
-            return ["200 rock %08x %s / %s" % (id, artist, title)]
-
-        # Found multiple matches
-        res = ["211 Found inexact matches, list follows (until terminating `.')"]
-        for id, title, artist in rows:
-            res.append("rock %08x %s / %s" % (id, artist, title))
-        res.append(".")
-        return res
-
     def handle_index(self):
         try:
 	    f = open(os.path.join(os.environ['STATIC_DIR'], "index"))
@@ -148,6 +42,15 @@ class CoverArtRedirect(object):
         if not filename:
             return ["400 no filename specified.", ""]
 
+        query = """
+            SELECT release.gid 
+              FROM release, release_gid_redirect 
+             WHERE release_gid_redirect.gid = %(mbid)s
+               AND new_id = release.id
+        """
+        rows = self.conn.execute(query, dict(mbid=mbid)).fetchall()
+        if rows:
+            mbid = rows[0][0];
         return ["307 Temporary Redirect", "http://archive.org/download/mbid-%s/mbid-%s-%s" % (mbid, mbid, filename)]
 
     def handle(self, environ):
