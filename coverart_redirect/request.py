bc66d14bb5cf87c973dc05b966d0db0ea8798ed9
Robert Kaye <rob@musicbrainz.org>
2011-10-26 21:14:29 -0700

    basic redirects are working. basic serving of the index page works.

--- coverart_redirect/request.py
+++ coverart_redirect/request.py
@@ -2,8 +2,11 @@
 # Copyright (C) 2011 Robert Kaye
 # Distributed under the MIT license, see the LICENSE file for details.
 
+import re
+import os
 import logging
 import coverart_redirect
+from wsgiref.util import shift_path_info, request_uri
 
 logger = logging.getLogger(__name__)
 
@@ -124,26 +127,38 @@ class CoverArtRedirect(object):
         res.append(".")
         return res
 
+    def handle_index(self):
+        try:
+	    f = open(os.path.join(os.environ['STATIC_DIR'], "index"))
+        except IOError:
+            return ['500 Internal Server Error', ""]
+
+        txt = f.read()
+        f.close()
+
+        return ['200 OK', txt]
+
     def handle_redirect(self, entity, mbid, filename):
         if not entity or entity != 'release':
             return ["400 Only release entities are supported currently", ""]
-        # TODO validate MBID
         if not mbid:
             return ["400 no MBID specified.", ""]
+        if not re.match('[a-f0-9]{8}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{4}-[a-f0-9]{8}', mbid):
+            return ["400 invalid MBID specified.", ""]
         if not filename:
             return ["400 no filename specified.", ""]
 
-        return ["307 Temporary Redirect", "http://download.archive.org/mbid-%s/mbid-%s-%s" % (mbid, mbid, filename)]
+        return ["307 Temporary Redirect", "http://archive.org/download/mbid-%s/mbid-%s-%s" % (mbid, mbid, filename)]
 
-#        self.cmd = self.cmd.lower().split()
-#        func_name = 'handle_cmd_' + self.cmd.pop(0)
-#        if hasattr(self, func_name):
-#            return getattr(self, func_name)()
-#
-#        return ["500 Command syntax error, command unknown, command unimplemented."]
-
-    def handle(self, entity, mbid, filename):
-        (code, response) = self.handle_redirect(entity, mbid, filename.encode('utf8')) 
+    def handle(self, environ):
+        entity = shift_path_info(environ)
+	if not entity:
+            return self.handle_index()
+        mbid = shift_path_info(environ)
+	if not mbid:
+            return ["400 invalid request.", ""]
+        filename = shift_path_info(environ)
+        (code, response) = self.handle_redirect(entity, mbid.lower(), filename.encode('utf8')) 
         logger.debug("Request %s %s %s:\n%s\n", entity, mbid, filename, response)
         return code, response
 
@@ -151,4 +166,3 @@ if __name__ == '__main__':
     from wsgiref.simple_server import make_server
     httpd = make_server('localhost', 8051, application)
     httpd.serve_forever()
-
--- coverart_redirect/server.py
+++ coverart_redirect/server.py
@@ -6,7 +6,6 @@ import logging
 import sqlalchemy
 from cgi import parse_qs
 from contextlib import closing
-from wsgiref.util import shift_path_info
 from coverart_redirect.config import Config
 from coverart_redirect.utils import LocalSysLogHandler
 from coverart_redirect.request import CoverArtRedirect
@@ -30,14 +29,21 @@ class Server(object):
             logging.getLogger().addHandler(handler)
 
     def __call__(self, environ, start_response):
-        entity = shift_path_info(environ)
-        mbid = shift_path_info(environ)
-        filename = shift_path_info(environ)
         with closing(self.engine.connect()) as conn:
             conn.execute("SET search_path TO musicbrainz")
-            status, location = CoverArtRedirect(self.config, conn).handle(entity, mbid, filename)
-        start_response(status, [('Location', location)])
-        return ""
+            status, txt = CoverArtRedirect(self.config, conn).handle(environ)
+        if status.startswith("307"):
+            start_response(status, [('Location', txt)])
+            return ""
+        elif status.startswith("200"):
+            start_response('200 OK', [
+                ('Content-Type', 'text/html; charset=UTF-8'),
+                ('Content-Length', str(len(txt)))])
+            return txt
+        else:
+            start_response('500 Oops', [])
+            return ""
+
 
 def make_application(config_path):
     app = Server(config_path)
