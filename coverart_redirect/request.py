a2058abba4311a9b0582643233091ef68671df90
warp <kuno@frob.nl>
2013-05-15 15:11:42 -0500

    CAA-60, Revert pointles release_group_meta join in resolve_release_group_cover_art query.

--- coverart_redirect/request.py
+++ coverart_redirect/request.py
@@ -138,8 +138,6 @@ class CoverArtRedirect(object):
                 ON release_group_cover_art.release = musicbrainz.release.id
               JOIN musicbrainz.release_group
                 ON musicbrainz.release_group.id = musicbrainz.release.release_group
-              JOIN musicbrainz.release_group_meta
-                ON musicbrainz.release_group_meta.id = musicbrainz.release_group.id
              WHERE release_group.gid = %(mbid)s
                AND is_front = true
           ORDER BY release.release_group, release_group_cover_art.release;
