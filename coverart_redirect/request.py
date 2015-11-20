f0c3b126b4031f5568e20e7940129e11a7c4f5e2
Roman Tsukanov <roman@tsukanov.me>
2015-11-20 14:57:36 +0100

    Fix copyright notices.

--- coverart_redirect/request.py
+++ coverart_redirect/request.py
@@ -2,7 +2,7 @@
 
 # Copyright (C) 2011 Lukas Lalinsky
 # Copyright (C) 2011 Robert Kaye
-# Copyright (C) 2015 MetaBrainz Foundation Inc.
+# Copyright (C) 2012, 2015 MetaBrainz Foundation Inc.
 #
 # Permission is hereby granted, free of charge, to any person obtaining a copy
 # of this software and associated documentation files (the "Software"), to deal
