818d10df26c86f856b034da66be00317cea77847
Roman Tsukanov <roman@tsukanov.me>
2017-08-13 05:21:43 +0200

    Add missing CSS cleaning

--- compile_resources.sh
+++ compile_resources.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
 cd "$(dirname $0)"
-node_modules/.bin/lessc ./static/css/main.less > ./static/css/main.css
+node_modules/.bin/lessc --clean-css ./static/css/main.less > ./static/css/main.css
